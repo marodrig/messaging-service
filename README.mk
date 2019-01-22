87a24641dcb0c29b8f6f575668558daa62fb98ae
Alexei Rodriguez <marodrig@alumni.purdue.edu>
2019-01-22 12:46:12 -0500

    feat: add README file.

---
+++ README.mk
@@ -0,0 +1,33 @@
+# Messaging Service
+
+## Table of contents
+
+## What is the reason for this project?
+
+## Why should you care about this project?
+
+## Features
+
+## Installation & Quick start
+
+### Built with & Requirements
+
+## Usage
+
+## REST-API reference
+
+### Status codes
+
+### Request arguments.
+
+### End-point reference
+
+## Running tests
+
+## FAQ
+
+## Contributing
+
+## Credits or References
+
+## License
--- src/message-service.py
+++ src/message-service.py
@@ -1,5 +1,9 @@
 """
 messaging service.
+Submit message to a recipient by a given id.
+Fetch messages not fetched previously
+Fetch messages by time and return a slice between start and stop indeces.
+Ordered by increasing or decreasing order.
 
 """
 import datetime
@@ -8,6 +12,7 @@ from flask import Flask, abort, jsonify, request, url_for
 
 app = Flask(__name__)
 fetched_messages = {}
+
 
 @app.route('/submit/', methods=['POST'])
 def submit_message(message):
@@ -19,19 +24,21 @@ def submit_message(message):
     Return:
 
     """
-    if 'message' and 'recipient-id' in request.POST:
-        message = request.POST.get('message', None)
-        recipient = request.POST.get('recipient-id', None)
+    if 'message' and 'recipient-id' in request.args:
+        request_json = request.get_json()
+        message = request_json.get('message', None)
+        recipient = request_json.get('recipient-id', None)
         time_message_sent = datetime.datetime.now().time()
         fetched_messages[(message, recipient)] = time_message_sent
         return jsonify(
-                        success=True,
+                        status=200,
                         recipient=recipient,
                         time_sent=time_message_sent)
     else:
-        return jsonify(success=False, error_code=404), 404
+        return jsonify(status=404, error='Error')
 
-@app.route('/fetch-messages/')
+
+@app.route('/fetch-messages/', methods=['GET'])
 def fetch_messages():
     """
     Fetch messages not fetched previously
@@ -41,11 +48,18 @@ def fetch_messages():
     Return:
 
     """
-    messages = []
-    for message in messages:
-        if message in fetch_messages.items():
-            return jsonify(success=True, messages=messages)
-    return jsonify(success=False, error=404), 404
+    if 'start-idx' and 'stop-idx' in request.args:
+        start_idx = request.args.get('start-idx', None)
+        stop_idx = request.args.get('stop-idx', None)
+        app.logger.debug('start-idx in HTTP request.')
+        app.logger.debug('start-idx: {0}'.format(start_idx))
+        app.logger.debug('start-idx: {0}'.format(stop_idx))
+    return jsonify(status=200, messages=['Great!', 'Awesome'])
+    # messages = []
+    # for message in messages:
+    #     if message in fetch_messages.items():
+    #         return jsonify(success=True, messages=messages)
+    # return jsonify(success=False, error=404), 404
 
 
 if __name__ == '__main__':
