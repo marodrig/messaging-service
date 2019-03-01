001e554ff83fc91963a690852ce1738e730372be
Alexei Rodriguez <marodrig@alumni.purdue.edu>
2019-02-28 20:54:46 -0500

    feat: add SOLID principles of OOP.

--- src/models.py
+++ src/models.py
@@ -3,14 +3,98 @@ ORM model for a message.
 
 """
 import datetime
-# import os
-# import sys
 
-from sqlalchemy import (Column, DateTime, Integer, String, create_engine)
+from sqlalchemy import Column, DateTime, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import (NoResultFound, ObjectDeletedError,
+                                StaleDataError)
 
+
+engine = create_engine('sqlite:///datastore.db')
 Base = declarative_base()
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
+
+class InternalServerError(Exception):
+    """
+    """
+    status_code = 500
+
+    def __init__(self, message, status_code=None, payload=None):
+        super().__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['code'] = self.__class__.__name__
+        rv['message'] = self.message
+        return rv
+
+
+class NotAcceptable(Exception):
+    """
+    """
+    status_code = 404
+
+    def __init__(self, message, status_code=None, payload=None):
+        super().__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['code'] = self.__class__.__name__
+        rv['message'] = self.message
+        return rv
+
+
+class NotFound(Exception):
+    """
+    """
+    status_code = 404
+
+    def __init__(self, message, status_code=None, payload=None):
+        super().__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['code'] = self.__class__.__name__
+        rv['message'] = self.message
+        return rv
+
+
+class InvalidUsage(Exception):
+    """
+    Exception class for invalid usage of the API
+
+    """
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        """
+        """
+        super().__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['code'] = self.__class__.__name__
+        rv['message'] = self.message
+        return rv
 
 # class Link(Base):
 #     """
@@ -35,9 +119,10 @@ Base = declarative_base()
 #     """
 #     __tablename__ = 'operation'
 
-
 class Message(Base):
     """
+    Message from user
+
     """
     __tablename__ = 'message'
     id = Column(Integer, primary_key=True)
@@ -51,9 +136,80 @@ class Message(Base):
         """
         return {
             'id': self.id,
-            'message_text': self.message_text,
-            'recipiend_id': self.recipient_id,
-            'date_sent': self.date_sent}
+            'messageText': self.message_text,
+            'recipientId': self.recipient_id,
+            'dateSent': self.date_sent}
+
+    @classmethod
+    def get(cls, request):
+        """
+        HTTP GET request
+
+        """
+
+        messages_qs = session.query(cls)
+        result = []
+        if 'Accept' in request.headers and 'application/json' not in request.headers.get('Accept', '').split(';'):
+            raise NotAcceptable('This API only supports application/json media type.', 406)
+        if 'startIdx' and 'stopIdx' in request.args:
+            start_idx = int(request.args.get('startIdx'))
+            stop_idx = int(request.args.get('stopIdx'))
+            if stop_idx < start_idx:
+                raise InvalidUsage('stopIdx must be greater than startIdx',
+                                   400)
+            try:
+                result = messages_qs.order_by('date_sent').slice(
+                                                    start_idx,
+                                                    stop_idx)
+            except NoResultFound:
+                raise NotFound('Query returned no messages.', 404)
+        else:
+            try:
+                result = messages_qs.all()
+            except NoResultFound:
+                raise NotFound('Query returned no messages', 404)
+        if result:
+            return [message.serialize for message in result]
+        else:
+            raise NotFound('Query return no messages.', 404)
+
+    @classmethod
+    def post(cls, request):
+        """
+        HTTP POST request
+
+        """
+        if request.is_json:
+            data = request.get_json()
+            message = cls(
+                message_text=data.get('message', None),
+                recipient_id=data.get('recipientId', None)
+            )
+            return message
+        else:
+            raise InvalidUsage('This service only accepts JSON data.', 400)
+
+    @classmethod
+    def delete(cls, request):
+        """
+        """
+        count_deleted = None
+        if 'messagesId' in request.args:
+            messages_ids = map(int, request.args.get('messagesId').split(','))
+            for message_id in messages_ids:
+                try:
+                    count_deleted = session.query(cls).filter_by(id=message_id).delete()
+                    session.commit()
+                except ObjectDeletedError:
+                    raise NotFound(
+                        'No message exists for messageId: {}'.format(message_id),
+                        404)
+                except StaleDataError as sde:
+                    raise InternalServerError(
+                        sde,
+                        500
+                    )
+        return {'deletedCount': count_deleted}
 
 
 engine = create_engine('sqlite:///datastore.db')
